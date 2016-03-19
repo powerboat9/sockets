f9dde05c8f46244cddc7827ff61fe7552b4503ca
powerboat9 <powerboat9@users.noreply.github.com>
2016-03-19 10:10:18 -0400

    Did some work on packet verification

--- main.lua
+++ main.lua
@@ -9,17 +9,40 @@ local packets = {
         return {
             check = "Yes, this is powerboat9's socket program",
             command = "connecting",
-            data = {phase = "display"}
+            data = {
+                phase = "display",
+                me = {
+                    ip = ip,
+                    socket = socket,
+                    protocol = protocol
+                }
+            }
         }
+    end,
+    attemptConnect = function
 }
+
+local connections = {}
+
+function verifyPacket(p)
+    if not ((type(p) == "table") and (type(p.data) == "table") and (type(p.data.me) == "table") and (type(p.data.me.ip) == "number")) then return false end
+    if not (type(p.me.socket) == "string") and (#socket == 16) then return false end
+    for _, v in gsub(p.me.socket) do
+        local num = string.byte(v)
+        if not (((num >= 97) and (num <= 122)) or ((num >= 48) and (num <= 57))) then return false end
+    end
+    return true
+end
 
 while true do
     event = {os.pullEvent()}
     coroutine.resume(user, unpack(event))
     if event[1] == "modem_message" then
         local msg, channel = event[4], event[3]
-        if (type(msg) == "table") and (msg.check == "Yes, this is powerboat9's socket program") and (type(msg.command) == "string") and (type(msg.data) == "table") then
+        if verifyPacket(msg) then
             if msg.command == "connecting" then
-                if (msg.data.phase = "display") and (not attempHandshake) or attemptHandshake(msg) then
+                if (msg.data.phase = "display") and ((not attempHandshake) or attemptHandshake(msg)) then
+                    table.insert(connections, {
+                        ip = data.me.ip
                     modem.transmit()
     coroutine.resume()
