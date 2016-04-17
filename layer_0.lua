02b4459a677974f984485a9213da1074eb09da7f
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-17 14:34:35 -0400

    Added ports

--- layer_0.lua
+++ layer_0.lua
@@ -1,5 +1,5 @@
 return {
-    function transmit(modem, msg, to, from, key, timestamp)
+    function transmit(modem, port, msg, to, from, key, timestamp)
         timestamp = timestamp or (os.time() + (os.day() * 24000))
         local hash = SHA.hash256(("%s:%s:%s:%s:%s"):format(to, from, timestamp, key, msg))
         modem.transmit(rednet.CHANNEL_BROADCAST, rednet.CHANNEL_BROADCAST, {
@@ -7,11 +7,12 @@ return {
             to = to,
             from = from,
             timestamp = timestamp,
+            port = port,
             hash = hash
         })
     end,
-    function interpret(msg, keytabel)
-        if (type(msg) == "table") and (type(msg.msg) == "string") and (type(msg.to) == "number") and (type(msg.from) == "number") and (type(msg.timestamp) == "number") then
+    function interpret(port, msg, keytabel)
+        if (type(msg) == "table") and (type(msg.msg) == "string") and (type(msg.to) == "number") and (type(msg.from) == "number") and (type(msg.timestamp) == "number") and (type(msg.port) == "number") then
             if SHA.hash256(("%s:%s:%s:%s:%s"):format(msg.to, msg.from, msg.timestamp, keytable[msg.from], msg.msg)) == msg.hash then
                 return msg, true
             end
