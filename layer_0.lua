49fca0c5f28575b56218ebbf5f718c387ce87a47
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-17 13:36:31 -0400

    Create layer_0.lua

---
+++ layer_0.lua
@@ -0,0 +1,21 @@
+return {
+    function transmit(modem, msg, to, from, timestamp, key)
+        local hash = SHA.hash256(("%s:%s:%s:%s:%s"):format(to, from, timestamp, key, msg))
+        modem.transmit(rednet.CHANNEL_BROADCAST, rednet.CHANNEL_BROADCAST, {
+            msg = msg,
+            to = to,
+            from = from,
+            timestamp = timestamp,
+            hash = hash
+        })
+    end,
+    function interpret(msg, keytabel)
+        if (type(msg) == "table") and (type(msg.msg) == "string") and (type(msg.to) == "number") and (type(msg.from) == "number") and (type(msg.timestamp) == "number") then
+            if SHA.hash256(("%s:%s:%s:%s:%s"):format(msg.to, msg.from, msg.timestamp, keytable[msg.from], msg.msg)) == msg.hash then
+                return msg, true
+            end
+            return msg, false
+        end
+        return false
+    end
+}
