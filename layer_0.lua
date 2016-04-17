3da0b1cd9aaf2330e42c8a936af0e5168825a331
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-17 14:44:06 -0400

    Fixed port bug

--- layer_0.lua
+++ layer_0.lua
@@ -12,7 +12,7 @@ return {
         })
     end,
     function interpret(port, msg, keytabel)
-        if (type(msg) == "table") and (type(msg.msg) == "string") and (type(msg.to) == "number") and (type(msg.from) == "number") and (type(msg.timestamp) == "number") and (type(msg.port) == "number") then
+        if (type(msg) == "table") and (type(msg.msg) == "string") and (type(msg.to) == "number") and (type(msg.from) == "number") and (type(msg.timestamp) == "number") and (msg.port == port) then
             if SHA.hash256(("%s:%s:%s:%s:%s"):format(msg.to, msg.from, msg.timestamp, keytable[msg.from], msg.msg)) == msg.hash then
                 return msg, true
             end
