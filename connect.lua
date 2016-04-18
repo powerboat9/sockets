c13cc9c7c32eb017235bb150793d9eeb8b663718
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-18 10:30:12 -0400

    Did stuff

--- connect.lua
+++ connect.lua
@@ -17,8 +17,12 @@ return {
             elseif e == "modem_message" then
                 local msg, signed = layer_0.interpret(from, port, msg, keytable) --Note: "from" is me, the connection is "from" me
                 if msg and (signed or (not keytable(msg.to)) then
-                    if msg.msg == "ACCEPT CONNECT" then
+                    if (msg.msg == "ACCEPT CONNECT") or (msg.msg == "OK BUT UNIDENTIFIED") then
                         return true
+                    elseif msg.msg == "UNIDENTIFIED" then
+                        return false, "unknown"
+                    elseif msg.msg == "UNSIGNED" then
+                        return false, "impositor"
                     elseif msg.msg == "DENY ATTEMPT" then
                         return false, "denied"
                     end
@@ -26,12 +30,12 @@ return {
             end
         end
     end,
-    listen = coroutine.create(function(modem, me, port, key, keytable, whitelist, blacklist, allowUnknown)
+    listen = coroutine.create(function(modem, me, port, key, keytable, blacklist, whitelist, allowUnknown)
         while true do
             local _, _, _, _, msg = os.pullEvent("modem_message")
             local msg, signed = layer_0.interpret(me, port, msg, keytable)
             local ok = false
-            if msg then
+            if msg and ((not blacklist) or (not blacklist[msg.from])) and ((not whitelist) or whitelist[msg.from]) then
                 if not signed then
                     if not keytable[msg.from] then
                         if allowUnknown then
@@ -45,11 +49,12 @@ return {
                     end
                 end
                 if ok then
+                    if msg.msg == "HELP" then
+                        
                     coroutine.yield(msg, signed)
                 end
             end
         end
     end),
-    
-                    
-                
+    function askKey(modem, me, helper, port, key, keytable)
+        
