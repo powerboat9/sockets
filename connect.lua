2fb867a63570155f5992d2fd83dddb4a4b88c70f
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-18 10:51:02 -0400

    Did some work

--- connect.lua
+++ connect.lua
@@ -1,7 +1,7 @@
 local layer_0 = loadfile("layer_0")
 
-return {
-    function connect(modem, port, to, from, key, keytable)
+local api = {
+    function connect(self, port, to, from, key)
         local timer = os.startTimer(5)
         local transmitTimer = os.startTimer(0.5)
         while true do
@@ -9,14 +9,14 @@ return {
             local e, timeID, _, _, msg = os.pullEvent()
             if e == "timer" then
                 if timeID == timer then
-                    layer_0.transmit(modem, port, "CONNECT FAIL", to, from, key)
+                    layer_0.transmit(self.modem, port, "CONNECT FAIL", to, from, key)
                     return false, "time-out"
                 elseif timeID == transmitTimer then
-                    layer_0.transmit(modem, port, "CONNECT", to, from, key)
+                    layer_0.transmit(self.modem, port, "CONNECT", to, from, key)
                 end
             elseif e == "modem_message" then
-                local msg, signed = layer_0.interpret(from, port, msg, keytable) --Note: "from" is me, the connection is "from" me
-                if msg and (signed or (not keytable(msg.to)) then
+                local msg, signed = layer_0.interpret(from, port, msg, self.keytable) --Note: "from" is me, the connection is "from" me
+                if msg and (signed or (not self.keytable(msg.to)) then
                     if (msg.msg == "ACCEPT CONNECT") or (msg.msg == "OK BUT UNIDENTIFIED") then
                         return true
                     elseif msg.msg == "UNIDENTIFIED" then
@@ -30,31 +30,37 @@ return {
             end
         end
     end,
-    listen = coroutine.create(function(modem, me, port, key, keytable, blacklist, whitelist, allowUnknown)
-        while true do
-            local _, _, _, _, msg = os.pullEvent("modem_message")
-            local msg, signed = layer_0.interpret(me, port, msg, keytable)
-            local ok = false
-            if msg and ((not blacklist) or (not blacklist[msg.from])) and ((not whitelist) or whitelist[msg.from]) then
-                if not signed then
-                    if not keytable[msg.from] then
-                        if allowUnknown then
-                            layer_0.transmit(modem, port, "OK BUT UNIDENTIFIED", msg.from, me, key)
-                            ok = true
+    function getListener(self, me, port, key, blacklist, whitelist, allowUnknown)
+        return coroutine.create(function()
+            while true do
+                local _, _, _, _, msg = os.pullEvent("modem_message")
+                local msg, signed = layer_0.interpret(me, port, msg, self.keytable)
+                local ok = false
+                if msg and ((not blacklist) or (not blacklist[msg.from])) and ((not whitelist) or whitelist[msg.from]) then
+                    if not signed then
+                        if not self.keytable[msg.from] then
+                            if allowUnknown then
+                                layer_0.transmit(self.modem, port, "OK BUT UNIDENTIFIED", msg.from, me, key)
+                                ok = true
+                            else
+                                layer_0.transmit(self.modem, port, "UNIDENTIFIED", msg.from, me, key)
+                            end
                         else
-                            layer_0.transmit(modem, port, "UNIDENTIFIED", msg.from, me, key)
+                            layer_0.transmit(self.modem, port, "UNSIGNED", msg.from, me, key)
                         end
-                    else
-                        layer_0.transmit(modem, port, "UNSIGNED", msg.from, me, key)
+                    end
+                    if ok then
+                        if (msg.msg == "HELP") and (port = 200) then
+                            layer_0.transmit(self.modem, port, "HELPING WITH: " .. textutils.serialize(keytable), msg.from, me, key)
+                        else
+                            coroutine.yield(msg, signed)
+                        end
                     end
                 end
-                if ok then
-                    if msg.msg == "HELP" then
-                        
-                    coroutine.yield(msg, signed)
-                end
             end
-        end
-    end),
-    function askKey(modem, me, helper, port, key, keytable)
+        end)
+    end,
+    function askKey(self, me, helper, key)
+        layer_0.transmit(self.modem, 200, "HELP", helper, me, key)
+        local listener = self:getListener(me, 200, key)
         
