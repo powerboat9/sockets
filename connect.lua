e3445b0c69b79c5a580ae4b201a994d0740274c0
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-18 09:57:03 -0400

    Added stuff

--- connect.lua
+++ connect.lua
@@ -2,30 +2,32 @@ local layer_0 = loadfile("layer_0")
 
 return {
     function connect(modem, port, to, from, key, keytable)
-        layer_0.transmit(modem, port, "CONNECT", to, from, key)
-        local broadcast = coroutine.create(function() layer_0.transmit(modem, port, "CONNECT", to, from, key); coroutine.yield() end)
         local timer = os.startTimer(5)
-        local listen = coroutine.create(function()
-            local returnValue = false
-            while true do
-                local e, timeID, _, _, msg = os.pullEvent()
-                if (e == "timer") and (timeID == timer) then
+        local transmitTimer = os.startTimer(0.5)
+        while true do
+            layer_0.transmit(modem, port, "CONNECT", to, from, key)
+            local e, timeID, _, _, msg = os.pullEvent()
+            if e == "timer" then
+                if timeID == timer then
                     layer_0.transmit(modem, port, "CONNECT FAIL", to, from, key)
-                    break
-                elseif e == "modem_message" then
-                    local msg, signed = layer_0.interpret(port, msg, keytable)
-                    if msg and (signed or (not keytable(msg.to)) then
-                        if msg.msg == "ACCEPT CONNECT" then
-                            returnValue = 
-                        elseif msg.msg == "DENY ATTEMPT" then
-                            coroutine.yield(true)
-                        else
-                            continue
-                        end
-                        break
+                    return false, "time-out"
+                elseif timeID == transmitTimer then
+                    layer_0.transmit(modem, port, "CONNECT", to, from, key)
+                end
+            elseif e == "modem_message" then
+                local msg, signed = layer_0.interpret(from, port, msg, keytable)
+                if msg and (signed or (not keytable(msg.to)) then
+                    if msg.msg == "ACCEPT CONNECT" then
+                        return true
+                    elseif msg.msg == "DENY ATTEMPT" then
+                        return false, "denied"
                     end
                 end
             end
         end
+    end,
+    listen = coroutine.create(function(me, port, whitelist, blacklist)
+        )
+            
                 
         
