4fc4a57c826371531144d5ddea4972c4b586e982
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-15 14:25:36 -0400

    Began work on RSA and Connections

--- layer_0.lua
+++ layer_0.lua
@@ -1,8 +1,40 @@
+local mInterface = false
+
 return {
-    function transmit(modem, port, msg, to, from, key, timestamp)
+    CHANNEL = 20000,
+    function setInterface(self, s)
+        if (type(s) == "string") and (peripheral.getType(s) == "modem") then
+            mInterface = peripheral.wrap(s)
+            mInterface.open(self.CHANNEL)
+            return true
+        end
+        return false
+    end,
+    function autoSetInterface(self)
+        local wire, wireless = {}, {}
+        peripheral.find("modem", function(n, h)
+            if h.isWireless() and (not wireless[1]) then
+                wireless[1] = h
+            elseif (not h.isWireless()) and (not wire[1]) then
+                wire[1] = h
+            end
+            return false
+        end)
+        if wireless[1] or wire[1] then
+            mInterface = wireless[1] or wire[1]
+            mInterface.open(self.CHANNEL)
+            return true
+        end
+        return false
+    end,
+    function rawSend(self, msg)
+        if not mInterface then self.autoSetInterface() end
+        mInterface.transmit(self.CHANNEL, self.CHANNEL, msg)
+    end,
+    function transmit(self, modem, port, msg, to, from, myPKey, timestamp)
         timestamp = timestamp or (os.time() + (os.day() * 24000))
-        local hash = SHA.hash256(("%s:%s:%s:%s:%s"):format(to, from, timestamp, key, msg))
-        modem.transmit(rednet.CHANNEL_BROADCAST, rednet.CHANNEL_BROADCAST, {
+        local hash = RSA.crypt(("%s:%s:%s:%s"):format(to, from, timestamp, msg), myPKey)
+        self.rawSend(rednet.CHANNEL_BROADCAST, rednet.CHANNEL_BROADCAST, {
             msg = msg,
             to = to,
             from = from,
@@ -11,13 +43,16 @@ return {
             hash = hash
         })
     end,
-    function interpret(me, port, msg, keytabel)
+    function interpret(self, me, port, msg, keytabel)
         if (type(msg) == "table") and (type(msg.msg) == "string") and (type(msg.to) == "number") and ((msg.to == me) or ((not me) and (type(msg.to) == "number")) and (type(msg.timestamp) == "number") and (msg.port == port) then
-            if SHA.hash256(("%s:%s:%s:%s:%s"):format(msg.to, msg.from, msg.timestamp, keytable[msg.from], msg.msg)) == msg.hash then
+            local hTO, hFrom, hTime, hMsg = 
+            if RSA.crypt(("%s:%s:%s:%s:%s"):format(msg.to, msg.from, msg.timestamp, msg.msg), keytable[msg.from]) == msg.hash then
                 return msg, true
             end
             return msg, false
         end
         return false
-    end
+    end,
+    function initiate(self, modem, myPrivKey, myPubKey, port, to, from)
+        self.rawSend({
 }
