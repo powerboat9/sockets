29ebb72e2a156ae9784fa0e478d58c97bce40b06
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-29 19:34:20 -0400

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -1,5 +1,9 @@
 local tGet = {}
 
+function tGet:check()
+    if not self.modem then
+        local wireless = peripheral.find("modem"
+
 function tGet:sendRSA(to, msg, port)
     if type(msg) ~= "string" then error("Could not send type " .. type(msg)) end
     self:checkRSA()
@@ -15,12 +19,12 @@ end
 
 function tGet:sendAES(to, msg, key)
     if type(msg) ~= "string" then error("Could not send type " .. type(msg), 2) end
-    self:checkAES()
+    self:check()
     self.modem.transmit(self.channel, self.channel, {
         _program = "PSockets",
         _encrypt = "AES",
         to = to,
-        from = self.ME
+        from = self.me
         port = port,
         msg = PCrypt.AES.crypt(msg, key),
     })
@@ -28,7 +32,12 @@ end
 
 function tGet:sendPlain(to, msg)
     if type(msg) ~= "string" then error("Could not send type " .. type(msg), 2) end
-    self:checkPlain()
+    self:check()
     self.modem.transmit(self.channel, self.channel, {
         _program = "PSockets",
-        
+        _encrypt = "plain",
+        _to = to,
+        _from = self.me,
+        _msg = msg,
+    })
+end
