e86ee73483aba0f75699dbd2003742096b1ff32c
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-29 18:06:57 -0400

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -1,4 +1,16 @@
 local tGet = {}
 
-function tGet:send(msg, key)
-    
+function tGet:sendRSA(to, msg, port)
+    self:check()
+    if type(msg) ~= "string" then error("Could not send type " .. type(msg)
+    self.modem.transmit(self.channel, self.channel, {
+        _program = "PSockets"'
+        _encrypt = "RSA"
+        to = to,
+        from = self.pubKey,
+        port = port,
+        msg = PCrypt.RSA.crypt(PCrypt.RSA.crypt(msg, to), self.privKey)
+    })
+end
+
+function tGet:sendAES(msg, key)
