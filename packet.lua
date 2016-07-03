68f5aaf0e1b44b45c31f3f002f063915ae358e2d
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-03 18:31:14 -0400

    Update packet.lua

--- packet.lua
+++ packet.lua
@@ -2,4 +2,5 @@ local function package(str, size)
     local ret = {}
     while true do
         if #str > size then
-            
+            ret[#ret + 1] = str:sub(1, size)
+            str = str 
