d8d6a1e457e453bfbf4cd828d8e3646549e1055e
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-04 00:21:01 -0400

    Update packet.lua

--- packet.lua
+++ packet.lua
@@ -17,14 +17,36 @@ local function package(str, size)
     return ret
 end
 
-function getRawPacket(str, encryption, to, from, n, key)
-    if type(str) ~= "string" then error("Message is a " .. type(str) .. ", expected a string", 2) end
-    local eMsg
-    if encryption == "RSA" then
-        and (encryption ~= "AES") then error("Invalid encryption type", 2) end
-    local packetRet = {
+local function bind(packets)
+    if type(packets) ~= "table" then error("Invalid packet list, got type " .. type(packets), 2) end
+    if #packets < 1 then error("Cannot combine zero packets", 2) end
+    for k, v in ipairs(packets) do
+        if type(v) ~= "table" then error("Invalid packet, got type " .. type(v), 2) end
+        v.size = #packets
+        v.n = k
+    end
+    return packets
+end
+
+local function getRawPacket(str, to, from)
+    if not PCrypt.convert.isH(str, 128) then
+        error("Message is not a 128 byte hexadecimal string", 2)
+    end
+    return {
         _pgram = " powerboat9:sockets",
         _version = version,
         to = to,
         from = from,
-        
+        msg = str,
+        msgID = PCrypt.convert.randH(32)
+    }
+end
+
+local function getRSAPackets(str, to, from, recevPubKey, sendPrivKey)
+    if type(str) ~= "string" then
+        error("Message is not a 128 byte hexadecimal string", 2)
+    end
+    if (type(recevPubKey) ~= "number") or (recevPubKey < 1) then error("Invalid public key", 2) end
+    if (type(sendPrivKey) ~= "number") or (sendPrivKey < 1) then error("Invalid private key", 2) end
+    eMsg = package(PCrypt.RSA.crypt(PCrypt.RSA.crypt(str, sendPrivKey), recevPubKey), 128)
+    
