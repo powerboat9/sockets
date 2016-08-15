ac2b24261450f5278b59a62d35d4f44fa6dc2ec3
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-15 11:14:54 -0400

    Update basic.lua

--- basic.lua
+++ basic.lua
@@ -1,3 +1,8 @@
+local interfaces = {}
+function addInterface(side)
+    if peripheral.getType(side) == "modem" then
+        local prefix = peripheral.call(side, "isWireless") ? "wlan" : "eth"
+
 function genListen(port, address)
     assert(isValidPort(port), "Invalid port")
     assert(isValidAdress(address) or (address == nil), "Invalid address")
@@ -5,4 +10,4 @@ function genListen(port, address)
         while true do
             local e = {os.pullEventRaw()}
             if e[1] == "modem_message" and type(e[5]) == "table" and e[5].port == port and (e[5].to == address or not address) then
-                coroutine.yield(true, e[5].msg, address ? nil : e[5]
+                coroutine.yield(true, e[5].msg, e[5].address)
