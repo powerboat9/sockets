95e7dfdafacbc7382c6ce46c127cdbc79925dbf2
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-15 09:08:35 -0400

    Create basic.lua

---
+++ basic.lua
@@ -0,0 +1,8 @@
+function genListen(port, address)
+    assert(isValidPort(port), "Invalid port")
+    assert(isValidAdress(address) or (address == nil), "Invalid address")
+    return coroutine.create(function()
+        while true do
+            local e = {os.pullEventRaw()}
+            if e[1] == "modem_message" and type(e[5]) == "table" and e[5].port == port and (e[5].to == address or not address) then
+                coroutine.yield(true, e[5].msg, address ? nil : e[5]
