820c6c5d9bef6e0244e2dcfaa0add921299cdd38
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-15 12:30:56 -0400

    Update basic.lua

--- basic.lua
+++ basic.lua
@@ -2,12 +2,43 @@ local interfaces = {}
 function addInterface(side)
     if peripheral.getType(side) == "modem" then
         local prefix = peripheral.call(side, "isWireless") ? "wlan" : "eth"
+        local n = #interfaces[prefix]
+        if not interfaces[prefix] = interfaces[prefix] or {}
+        interfaces[prefix][n] = modem
+        return prefix .. n
+    else
+        return false
+    end
+end
 
+local verifyMsg(e, port, address)
+    assert(isValidPort(port) or port == nil, "Invalid port")
+    assert(isValidAddress(address) or address == nil, "Invalid address")
+    return e[1] == "modem_message" and type(e[5]) == "table" and (e[5].port == port or not port) and (e[5].to == address or not address) and (isValidPort(e[5].retPort) or e[5].retPort == nil) and isValidAddress(e[5].from) and isValidMsgId(e[5].msgID)
+end
+
+local function sndBack(e)
+    if verifyMsg(e) then
+        return ({pcall(function() peripheral.call(e[2], "transmit", e[4], e[3], {
+            type = "verify",
+            port = e[5].retPort or e[5].port,
+            to = e[5].from,
+            from = e[5].to,
+            msgID = e[5].msgID
+        }) end)})[1]
+    end
+    return false
+end
+
+local connections = {}
 function genListen(port, address)
     assert(isValidPort(port), "Invalid port")
     assert(isValidAdress(address) or (address == nil), "Invalid address")
     return coroutine.create(function()
         while true do
             local e = {os.pullEventRaw()}
-            if e[1] == "modem_message" and type(e[5]) == "table" and e[5].port == port and (e[5].to == address or not address) then
-                coroutine.yield(true, e[5].msg, e[5].address)
+            if verifyMsg(e, port, address) then
+                if e[5].type == "msg" then
+                    coroutine.yield(true, "msg", e[5].msg, e[5].address)
+                elseif e[5].type == "connect" then
+                    
