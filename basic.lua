40aac4b092992eb2063e8cfa5220ca334ece8a44
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-15 12:48:22 -0400

    Update basic.lua

--- basic.lua
+++ basic.lua
@@ -1,3 +1,6 @@
+math.randomseed(os.time()); math.random(); math.random()
+local function log(msg) end
+
 local interfaces = {}
 function addInterface(side)
     if peripheral.getType(side) == "modem" then
@@ -17,15 +20,26 @@ local verifyMsg(e, port, address)
     return e[1] == "modem_message" and type(e[5]) == "table" and (e[5].port == port or not port) and (e[5].to == address or not address) and (isValidPort(e[5].retPort) or e[5].retPort == nil) and isValidAddress(e[5].from) and isValidMsgId(e[5].msgID)
 end
 
+local callbacks = {}
+local setupCallback(funct, timesLeft)
+    local t = os.startTimer(1)
+    callbacks[t] = function()
+        local ok, err = pcall(funct)
+        if not OK then
+            log("Callback failed, " .. (timesLeft + 1) .. " tries left"
+
 local function sndBack(e)
     if verifyMsg(e) then
-        return ({pcall(function() peripheral.call(e[2], "transmit", e[4], e[3], {
+        local ok = pcall(function() peripheral.call(e[2], "transmit", e[4], e[3], {
             type = "verify",
             port = e[5].retPort or e[5].port,
             to = e[5].from,
             from = e[5].to,
             msgID = e[5].msgID
-        }) end)})[1]
+        }) end)
+        if ok then
+            local t = os.startTimer(1)
+            callbacks[t] = function
     end
     return false
 end
