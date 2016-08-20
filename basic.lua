e31eeb0eda4676775f9a1716791d6c1c9f48f412
powerboat9 <powerboat9@users.noreply.github.com>
2016-08-20 11:21:04 -0400

    Update basic.lua

--- basic.lua
+++ basic.lua
@@ -17,17 +17,39 @@ function addInterface(side)
         local prefix = peripheral.call(side, "isWireless") ? "wlan" : "eth"
         local n = #interfaces[prefix]
         if not interfaces[prefix] = interfaces[prefix] or {}
-        interfaces[prefix][n] = modem
+        interfaces[prefix][n] = {modem = peripheral.wrap(side), connections = {}, side = side}
         return prefix .. n
     else
         return false
     end
 end
 
+local function findInterface(side)
+    local isOk = false
+    for _, checkSide in ipairs(rs.getSides()) do
+        if side == checkSide then
+            isOk = true
+            break
+        end
+    end
+    if not isOk then
+        return false
+    end
+    for prefix in pairs(interfaces) do
+        for n in pairs(interfaces[prefix]) do
+            local interface = interfaces[prefix][n].side
+            if interface.side == side then
+                return interface
+            end
+        end
+    end
+    return false
+end
+
 local verifyMsg(e, port, address)
     assert(isValidPort(port) or port == nil, "Invalid port")
     assert(isValidAddress(address) or address == nil, "Invalid address")
-    return e[1] == "modem_message" and type(e[5]) == "table" and (e[5].port == port or not port) and (e[5].to == address or not address) and (isValidPort(e[5].retPort) or e[5].retPort == nil) and isValidAddress(e[5].from) and isValidMsgId(e[5].msgID)
+    return e[1] == "modem_message" and type(e[5]) == "table" and e[5]._pgram == "PSocket" and (e[5].port == port or not port) and (e[5].to == address or not address) and (isValidPort(e[5].retPort) or e[5].retPort == nil) and isValidAddress(e[5].from) and isValidMsgId(e[5].msgID)
 end
 
 local function updateTimer(t)
@@ -81,14 +103,47 @@ local function sndBack(e)
     end, 9)
 end
 
+local function genTransmitBackFunction(e)
+    if not verifyMsg(e) then
+        return false
+    end
+    return function(msg)
+        e[
+
+local function startConnection(e)
+    if not verifyMsg(e) or e[5].type ~= "startConnection" then
+        return false
+    end
+    local interface = findInterface(e[2])
+    if not interface then
+        return false
+    end
+    local connection = {
+        otherConnectionID = e[5].connectID,
+        transmit = genTransmitBackFunction(e)
+    }
+    sendBack(e, {
+        _pgram = "PSocket",
+        type = "okConnection",
+        connectID = e[5].connectID)
+
+local function broadcastKeepAlives(interface)
+    for prefix in pairs(interfaces) do
+        for n in pairs(interfaces[prefix]) do
+            for _, connection in ipairs(interfaces[prefix][n].connections) do
+                connection.send({
+                    _pgram = "PSocket",
+                    type = "keepAlive"
+                })
+            end
+        end
+    end
+end
+
 function genListen(port, address)
     assert(isValidPort(port), "Invalid port")
     assert(isValidAdress(address) or (address == nil), "Invalid address")
     return coroutine.create(function()
-        local connections = {
-            list = {},
-            timers = {}
-        }
         while true do
             local e = {os.pullEventRaw()}
             if e[1] == "timer" then
